d9e6a863096fbda0758df52ff24ab2db345e1605
Taylor Holberton <taylorcholberton@gmail.com>
2017-01-14 13:35:40 -0800

    removed change log and added normal header guard

--- src/audio-driver.h
+++ src/audio-driver.h
@@ -1,40 +1,5 @@
-/* Common definitions for audio drivers. (c) 2006 Antti S. Lankila
- * Licensed under GPL.
- * $Id$
- *
- * $Log$
- * Revision 1.7  2006/08/23 08:11:23  alankila
- * - add unlikely to hopefully speed up if() test
- *
- * Revision 1.6  2006/08/22 06:40:05  alankila
- * - inform gcc on the rare case where it may be helpful that
- *   data and data_swap pointers do not alias.
- *
- * Revision 1.5  2006/08/10 16:18:36  alankila
- * - improve const correctness and make gnuitar compile cleanly under
- *   increasingly pedantic warning models.
- *
- * Revision 1.4  2006/08/08 21:05:31  alankila
- * - optimize gnuitar: this breaks dsound, I'll fix it later
- *
- * Revision 1.3  2006/08/07 12:55:30  alankila
- * - construct audio-driver.c to hold globals and provide some utility
- *   functions to its users. This slashes interdependencies somewhat.
- *
- * Revision 1.2  2006/08/06 20:45:52  alankila
- * - use stdint type rather than glib type
- *
- * Revision 1.1  2006/08/06 20:14:54  alankila
- * - split pump.h into several domain-specific headers to reduce file
- *   interdependencies (everyone included pump.h). New files are:
- *   - effect.h for effect definitions
- *   - audio-driver.h for work relating to audio drivers
- *   - audio-midi.h for MIDI interaction.
- *
- */
-
-#ifndef _AUDIO_DRIVER_H_
-#define _AUDIO_DRIVER_H_ 1
+#ifndef GNUITAR_AUDIO_DRIVER_H
+#define GNUITAR_AUDIO_DRIVER_H
 
 #include <stdint.h>
 
