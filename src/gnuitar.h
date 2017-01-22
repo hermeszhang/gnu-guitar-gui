dabc02c877cdb83a075fb9873a89876a055b63de
Taylor Holberton <taylorcholberton@gmail.com>
2017-01-22 09:29:52 -0800

    added top level header

---
+++ src/gnuitar.h
@@ -0,0 +1,12 @@
+#ifndef GNUITAR_H
+#define GNUITAR_H
+
+#include "audio-driver.h"
+#include "effect.h"
+#include "package.h"
+#include "packet.h"
+#include "pump.h"
+#include "version.h"
+
+#endif /* GNUITAR_H */
+
