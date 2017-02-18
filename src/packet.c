e94b5a3d8e98ee7d0e5f06edcaab7dda33d18574
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-17 21:05:46 -0800

    added const specifier

--- src/packet.c
+++ src/packet.c
@@ -1,8 +1,8 @@
 #include "packet.h"
 
-float gnuitar_sample_max = GNUITAR_SAMPLE_MAX;
+const float gnuitar_sample_max = GNUITAR_SAMPLE_MAX;
 
-float gnuitar_sample_min = GNUITAR_SAMPLE_MIN;
+const float gnuitar_sample_min = GNUITAR_SAMPLE_MIN;
 
 void
 gnuitar_packet_mul(struct GnuitarPacket *packet, float n)
