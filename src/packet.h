2976144d279249b5de2fce2bdc706926bc37ee10
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-18 12:51:11 -0800

    using int32_t for packet sample type

--- src/packet.h
+++ src/packet.h
@@ -9,27 +9,25 @@
 #define __restrict__
 #endif
 
-#define MAX_SAMPLE (32767 << 8)
+#define GNUITAR_SAMPLE_MAX INT32_MAX
 
-#define GNUITAR_SAMPLE_MAX (INT16_MAX * (1 << 8))
+#define GNUITAR_SAMPLE_MIN INT32_MIN
 
-#define GNUITAR_SAMPLE_MIN (INT16_MIN * (1 << 8))
+extern const int32_t gnuitar_sample_max;
 
-extern const double gnuitar_sample_max;
-
-extern const double gnuitar_sample_min;
+extern const int32_t gnuitar_sample_min;
 
 struct GnuitarPacket {
-    double * __restrict__ data;
-    double * __restrict__ data_swap;
+    int32_t * __restrict__ data;
+    int32_t * __restrict__ data_swap;
     unsigned int len;
     unsigned int channels;
     unsigned long int rate;
 };
 
-void gnuitar_packet_mul(struct GnuitarPacket *packet, double n);
+void gnuitar_packet_mul(struct GnuitarPacket *packet, int32_t n);
 
-void gnuitar_packet_div(struct GnuitarPacket *packet, double n);
+void gnuitar_packet_div(struct GnuitarPacket *packet, int32_t n);
 
 #endif /* GNUITAR_PACKET_H */
 
