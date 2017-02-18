99836d88b74180a89fdaf6726fb616f4d571dff8
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-17 21:05:34 -0800

    renamed audio-driver.h to track.h and removed package.h

--- src/gnuitar.h
+++ src/gnuitar.h
@@ -6,11 +6,10 @@
  * Welcome to the Gnuitar API.
  */
 
-#include "audio-driver.h"
 #include "effect.h"
-#include "package.h"
 #include "packet.h"
 #include "pump.h"
+#include "track.h"
 #include "version.h"
 
 #endif /* GNUITAR_H */
