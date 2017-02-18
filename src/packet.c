42c906941b2517de5886ee5dc870f3e9e9554bb0
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-18 13:19:07 -0800

    added documentation to packet

--- src/packet.c
+++ src/packet.c
@@ -4,19 +4,68 @@ const int32_t gnuitar_sample_max = GNUITAR_SAMPLE_MAX;
 
 const int32_t gnuitar_sample_min = GNUITAR_SAMPLE_MIN;
 
+/** Multiplies the samples in the packet by a signed integer.
+ * This may cause the samples to clip.
+ * @param packet An initialized packet.
+ * @param n A signed integer multiplying factor.
+ *  This number is clipped if it is greater
+ *  than @ref GNUITAR_SAMPLE_MAX or less than
+ *  @ref GNUITAR_SAMPLE_MIN.
+ * @ingroup libgnuitar-packet
+ */
+
 void
-gnuitar_packet_mul(struct GnuitarPacket *packet, int32_t n)
+gnuitar_packet_mul_si(struct GnuitarPacket *packet, int32_t n)
 {
-    unsigned int i;
+    size_t i;
+    int64_t product;
+
+#if GNUITAR_SAMPLE_MAX != INT32_MAX
+    if (n > GNUITAR_SAMPLE_MAX)
+        n = GNUITAR_SAMPLE_MAX;
+#endif /* GNUITAR_SAMPLE_MAX != INT32_MAX */
+
+#if GNUITAR_SAMPLE_MIN != INT32_MIN
+    if (n < GNUITAR_SAMPLE_MIN)
+        n = GNUITAR_SAMPLE_MIN;
+#endif /* GNUITAR_SAMPLE_MIN != INT32_MIN */
+
     for (i = 0; i < packet->len; i++) {
-        packet->data[i] *= n;
+        product = packet->data[i];
+	product *= n;
+	if (product > GNUITAR_SAMPLE_MAX)
+	    packet->data[i] = GNUITAR_SAMPLE_MAX;
+	else if (product < GNUITAR_SAMPLE_MIN)
+	    packet->data[i] = GNUITAR_SAMPLE_MIN;
+	else
+	    packet->data[i] = product;
     }
 }
 
+/** Divides the samples in the packet by a signed integer.
+ * @param packet An initialized packet
+ * @param n A signed integer divisor
+ *  This number is clipped if it is greater
+ *  than @ref GNUITAR_SAMPLE_MAX or less than
+ *  @ref GNUITAR_SAMPLE_MIN.
+ * @ingroup libgnuitar-packet
+ */
+
 void
-gnuitar_packet_div(struct GnuitarPacket *packet, int32_t n)
+gnuitar_packet_div_si(struct GnuitarPacket *packet, int32_t n)
 {
-    unsigned int i;
+    size_t i;
+
+#if GNUITAR_SAMPLE_MAX != INT32_MAX
+    if (n > GNUITAR_SAMPLE_MAX)
+        n = GNUITAR_SAMPLE_MAX;
+#endif /* GNUITAR_SAMPLE_MAX != INT32_MAX */
+
+#if GNUITAR_SAMPLE_MIN != INT32_MIN
+    if (n < GNUITAR_SAMPLE_MIN)
+        n = GNUITAR_SAMPLE_MIN;
+#endif /* GNUITAR_SAMPLE_MIN != INT32_MIN */
+
     for (i = 0; i < packet->len; i++) {
         packet->data[i] /= n;
     }
--- src/packet.h
+++ src/packet.h
@@ -2,10 +2,15 @@
 #define GNUITAR_PACKET_H
 
 #include <stdint.h>
+#include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
+/** @defgroup libgnuitar-packet Audio Packets
+ * The API related to audio packets.
+ */
 
 /* get rid of __restrict__ type qualifier for MS Visual C */
 
@@ -13,25 +18,54 @@ extern "C" {
 #define __restrict__
 #endif
 
+/** The maximum value of an audio sample
+ * @ingroup libgnuitar-packet
+ */
+
 #define GNUITAR_SAMPLE_MAX INT32_MAX
+
+/** The minimum value of an audio sample
+ * @ingroup libgnuitar-packet
+ */
 
 #define GNUITAR_SAMPLE_MIN INT32_MIN
 
+/** The maximum value of an audio sample, as a variable
+ * @ingroup libgnuitar-packet
+ */
+
 extern const int32_t gnuitar_sample_max;
+
+/** The minimum value of an audio sample, as a variable
+ * @ingroup libgnuitar-packet
+ */
 
 extern const int32_t gnuitar_sample_min;
 
+/** An audio packet.
+ * Audio packets are preferably manipulated, instead of samples,
+ * so that effects and audio drivers are not dependent on the sample type.
+ * @ingroup libgnuitar-packet
+ */
+
 struct GnuitarPacket {
+    /** Audio samples in the packet.
+     * The samples are interleaved. */
     int32_t * __restrict__ data;
+    /** A swap buffer for the packet.
+     * The samples are interleaved. */
     int32_t * __restrict__ data_swap;
-    unsigned int len;
-    unsigned int channels;
-    unsigned long int rate;
+    /** The number of frames in the packet. */
+    size_t len;
+    /** The number of channels per frame. */
+    size_t channels;
+    /** The number of frames per second. */
+    size_t rate;
 };
 
-void gnuitar_packet_mul(struct GnuitarPacket *packet, int32_t n);
+void gnuitar_packet_mul_si(struct GnuitarPacket *packet, int32_t n);
 
-void gnuitar_packet_div(struct GnuitarPacket *packet, int32_t n);
+void gnuitar_packet_div_si(struct GnuitarPacket *packet, int32_t n);
 
 #ifdef __cplusplus
 } /* extern "C" { */
