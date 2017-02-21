e9f34368959578b59c553f3e55005ca960c95109
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-20 18:16:27 -0800

    added call to cpack

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -e "scripts/cmake-build.sh" ]; then
+if [ ! -e "scripts/travis-build.sh" ]; then
     echo "must be run from top level directory"
     exit 1
 fi
 
 mkdir -p build && cd build
 if [ $? != 0 ]; then
-  exit 1
+    exit 1
 fi
 
 cmake ..
@@ -16,6 +16,11 @@ if [ $? != 0 ]; then
 fi
 
 cmake --build .
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+cpack -G TGZ
 if [ $? != 0 ]; then
     exit 1
 fi
