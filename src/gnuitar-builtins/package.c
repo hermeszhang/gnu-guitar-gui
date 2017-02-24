15d49c5346c81b5ae47862a32a21e6ba88b5b67e
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-23 20:15:12 -0800

    Added package entry point

---
+++ src/gnuitar-builtins/package.c
@@ -0,0 +1,27 @@
+#include "../gnuitar.h"
+
+#include "echo.h"
+
+const struct GnuitarPackageEffect builtin_effects[] = {
+    { "Echo", gnuitar_echo_init }
+};
+
+int
+gnuitar_package_entry(struct GnuitarPackage *package)
+{
+    size_t i;
+    size_t builtin_effects_count;
+    int err;
+
+    builtin_effects_count = sizeof(builtin_effects);
+    builtin_effects_count /= sizeof(builtin_effects[0]);
+
+    for (i = 0; i < builtin_effects_count; i++) {
+        err = gnuitar_package_add_effect(package, &builtin_effects[i]);
+        if (err != 0)
+            return err;
+    }
+
+    return 0;
+}
+
