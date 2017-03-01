e2b1f812b153d012669a38b67945c651779dbc08
Taylor Holberton <taylorcholberton@gmail.com>
2017-03-01 05:47:48 -0500

    removed unused code

--- src/track.h
+++ src/track.h
@@ -55,34 +55,6 @@ int gnuitar_track_start(struct GnuitarTrack *track);
 
 int gnuitar_track_stop(struct GnuitarTrack *track);
 
-/* for compatibility */
-
-#ifdef _WIN32
-#define MAX_BUFFERS 1024 /* number of input/output sound buffers */
-#endif
-
-#define MIN_BUFFER_SIZE 128
-#define MAX_BUFFER_SIZE 65536
-#define MAX_CHANNELS 4
-#define MAX_SAMPLE_RATE 48000
-
-#ifdef _WIN32
-extern unsigned int nbuffers;
-extern unsigned int overrun_threshold;
-#endif
-
-#ifdef _WIN32
-/* sadly, Windows and Linux have a different idea what the size of the buffer is.
- * Linux world talks about size in frames because that is most convenient for ALSA
- * and JACK (but less so for OSS). */
-extern gnuitar_sample_t procbuf[MAX_BUFFER_SIZE / sizeof(int16_t)];
-extern gnuitar_sample_t procbuf2[MAX_BUFFER_SIZE / sizeof(int16_t)];
-#endif
-
-void guess_track(void);
-void set_track_from_str(const char *str);
-void triangular_dither(struct GnuitarPacket *db, int16_t *target);
-
 #ifdef __cplusplus
 } /* extern "C" { */
 #endif /* __cplusplus */
