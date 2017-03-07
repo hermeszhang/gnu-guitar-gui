0c140c0bcaaf5147c9a44e883a7cc24535355184
Taylor Holberton <taylorcholberton@gmail.com>
2017-03-06 21:34:13 -0800

    using plugin manager to add an effect

--- src/libgnuitar/driver-test.cc
+++ src/libgnuitar/driver-test.cc
@@ -16,19 +16,19 @@ main (void)
   if (driver == nullptr)
     return EXIT_FAILURE;
 
-#ifdef __unix__
-  Gnuitar::Plugin plugin ("plugins/caps.so");
-  if (plugin.good ())
+  driver->set_input("plughw:1,0");
+
+  Gnuitar::LADSPA::PluginManager plugin_manager;
+  plugin_manager.parse_env();
+  plugin_manager.find_all_plugins();
+  auto effect = plugin_manager.get_effect("C* Scape - Stereo delay with chromatic resonances");
+  if (effect != nullptr)
     {
-      auto effect = plugin.get_effect (0);
-      if (effect != nullptr)
-        {
-          std::cout << "using effect: " << effect->get_name () << std::endl;
-          effect->activate(48000);
-          driver->add_effect(effect);
-        }
+      std::cout << "using effect: " << effect->get_name () << std::endl;
+      effect->instantiate(driver->get_rate());
+      effect->activate();
+      driver->add_effect(effect);
     }
-#endif /* __unix__ */
 
   driver->start();
 
