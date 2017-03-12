8c8a33c08de2b7ce3e2cb70cc189c29b046c84eb
tay10r <taylorcholberton@gmail.com>
2017-03-12 15:05:24 -0700

    reformatted code and resizing window

--- src/gnuitar-qt/gnuitar-qt.cc
+++ src/gnuitar-qt/gnuitar-qt.cc
@@ -18,49 +18,52 @@
 int
 main(int argc, char *argv[])
 {
+
 #ifdef __linux__
 
-    /* increase the priority of the process for lower latency */
+  /* increase the priority of the process for lower latency */
 
-    int max_priority = sched_get_priority_max(SCHED_FIFO);
+  int max_priority = sched_get_priority_max(SCHED_FIFO);
 
-    struct sched_param p;
+  struct sched_param p;
 
-    p.sched_priority = max_priority / 2;
+  p.sched_priority = max_priority / 2;
 
-    if (sched_setscheduler(0, SCHED_FIFO, &p)){
-        const char * dbgMsg = "warning: unable to set realtime priority (needs root privileges)";
-        qDebug() << dbgMsg;
+  if (sched_setscheduler (0, SCHED_FIFO, &p))
+    {
+      const char * dbgMsg = "warning: unable to set realtime priority (needs root privileges)";
+      qDebug() << dbgMsg;
     }
 
     /* We might have been running to this point as setuid root program.
      * Switching to real user id. */
 
-    if (setuid(getuid()) != 0) {
-        const char * dbgMsg = "error: unable to drop root privileges";
-	qDebug() << dbgMsg;
-	return EXIT_FAILURE;
+  if (setuid (getuid ()) != 0)
+    {
+      const char * dbgMsg = "error: unable to drop root privileges";
+      qDebug() << dbgMsg;
+      return EXIT_FAILURE;
     }
 
 #endif /* __linux__ */
 
-    /* Some FPU flags for faster performance. It is likely that
-     * -ffast-math already generates code to turn these on, though. */
+  /* Some FPU flags for faster performance. It is likely that
+   * -ffast-math already generates code to turn these on, though. */
 
 #ifdef __SSE__
-    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+  _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
 #endif /* __SSE__ */
 
 #ifdef __SSE3__
-    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+  _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
 #endif /* __SSE3__ */
 
-    QApplication a(argc, argv);
+  QApplication a(argc, argv);
 
-    Gnuitar::Qt::MainWindow w;
+  Gnuitar::Qt::MainWindow w;
+  w.resize (640, 480);
+  w.show();
 
-    w.show();
-
-    return a.exec();
+  return a.exec();
 }
 
