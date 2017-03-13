ff3ee3ec3eb4d428938b0a84d24c0776eb91207b
tay10r <taylorcholberton@gmail.com>
2017-03-12 19:25:55 -0700

    added circuit parsing test

--- src/gnuitar-ampc/spice-parser-test.cc
+++ src/gnuitar-ampc/spice-parser-test.cc
@@ -9,6 +9,9 @@ main (void)
 {
   std::stringstream source;
   source << "Rswamp 1 3 200" << std::endl;
+  source << "C1 3 4 1.00" << std::endl;
+  source << "Rload 4 2 200" << std::endl;
+  source << "Cbypass 2 1 1.00" << std::endl;
 
   Gnuitar::AmpC::SpiceLexer lexer (source);
 
@@ -16,11 +19,20 @@ main (void)
 
   Gnuitar::AmpC::Resistor rswamp;
   parser.read (rswamp);
-
   assert (rswamp.get_label () == "swamp");
   assert (rswamp.get_input () == 1);
   assert (rswamp.get_output () == 3);
   assert (static_cast<int> (rswamp.get_resistance ()) == 200);
+
+  Gnuitar::AmpC::Capacitor c1;
+  parser.read (c1);
+  assert (c1.get_label () == "1");
+  assert (c1.get_input () == 3);
+  assert (c1.get_output () == 4);
+  assert (static_cast<int> (c1.get_capacitance ()) == 1);
+
+  Gnuitar::AmpC::Circuit circuit;
+  parser.read (circuit);
 
   return 0;
 }
