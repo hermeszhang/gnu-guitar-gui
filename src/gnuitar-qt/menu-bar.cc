b69ebc572cde08bcd7268ea209d7abc4a09c9ac9
tay10r <taylorcholberton@gmail.com>
2017-03-13 22:08:20 -0700

    removed core library code and reformatted

--- src/gnuitar-qt/menu-bar.cc
+++ src/gnuitar-qt/menu-bar.cc
@@ -6,99 +6,92 @@
 #define GNUITAR_ISSUES_LINK "https://github.com/gnuitar/gnuitar/issues/new"
 #endif /* GNUITAR_ISSUES_LINK */
 
-namespace Gnuitar {
-
-namespace Qt {
-
-MenuBar::MenuBar(QWidget *parent) : QMenuBar(parent)
+namespace Gnuitar
 {
-    file_menu = addMenu("File");
-    open_preset_action = file_menu->addAction("Open Preset");
-    save_preset_action = file_menu->addAction("Save Preset");
-    file_menu->addSeparator();
-    quit_action = file_menu->addAction("Quit");
-    connect(quit_action, &QAction::triggered, this, &MenuBar::quit_triggered);
 
-    edit_menu = addMenu("Edit");
-    undo_action = edit_menu->addAction("Undo");
-    redo_action = edit_menu->addAction("Redo");
-    edit_menu->addSeparator();
-    preferences_action = edit_menu->addAction("Preferences");
+namespace Qt
+{
 
-    view_menu = addMenu("View");
-    zoom_in_action = view_menu->addAction("Zoom In");
-    zoom_out_action = view_menu->addAction("Zoom Out");
-    normal_size_action = view_menu->addAction("Normal Size");
-    view_menu->addSeparator();
-    fullscreen_action = view_menu->addAction("Full Screen");
+MenuBar::MenuBar (QWidget *parent) : QMenuBar (parent)
+{
+  file_menu = addMenu ("File");
+  open_preset_action = file_menu->addAction ("Open Preset");
+  save_preset_action = file_menu->addAction ("Save Preset");
+  file_menu->addSeparator ();
+  quit_action = file_menu->addAction ("Quit");
+  connect (quit_action, &QAction::triggered, this, &MenuBar::on_quit_selected);
 
-    plugins_menu = addMenu("Plugins");
-    ladspa_plugins_menu = plugins_menu->addMenu("LADSPA");
-    lv2_plugins_menu = plugins_menu->addMenu("LV2");
+  edit_menu = addMenu ("Edit");
+  undo_action = edit_menu->addAction ("Undo");
+  redo_action = edit_menu->addAction ("Redo");
+  edit_menu->addSeparator ();
+  preferences_action = edit_menu->addAction ("Preferences");
 
-    help_menu = addMenu("Help");
-    view_documentation_action = help_menu->addAction("View Documentation");
-    report_an_issue_action = help_menu->addAction("Report an Issue");
-    help_menu->addSeparator();
-    about_action = help_menu->addAction("About");
-    connect(view_documentation_action, &QAction::triggered, this, &MenuBar::view_documentation_triggered);
-    connect(report_an_issue_action, &QAction::triggered, this, &MenuBar::report_an_issue_triggered);
-    connect(about_action, &QAction::triggered, this, &MenuBar::about_triggered);
+  view_menu = addMenu ("View");
+  zoom_in_action = view_menu->addAction ("Zoom In");
+  zoom_out_action = view_menu->addAction ("Zoom Out");
+  normal_size_action = view_menu->addAction ("Normal Size");
+  view_menu->addSeparator ();
+  fullscreen_action = view_menu->addAction ("Full Screen");
 
-    doc_browser = new DocBrowser();
-    doc_browser->hide();
+  effects_menu = addMenu ("Effects");
+
+  help_menu = addMenu ("Help");
+  view_documentation_action = help_menu->addAction ("View Documentation");
+  report_an_issue_action = help_menu->addAction ("Report an Issue");
+  help_menu->addSeparator ();
+  about_action = help_menu->addAction ("About");
+  connect (view_documentation_action, &QAction::triggered, this, &MenuBar::on_view_documentation_selected);
+  connect (report_an_issue_action, &QAction::triggered, this, &MenuBar::on_report_an_issue_selected);
+  connect (about_action, &QAction::triggered, this, &MenuBar::on_about_selected);
+
+  doc_browser = new DocBrowser ();
+  doc_browser->hide ();
 }
 
-MenuBar::~MenuBar(void)
+MenuBar::~MenuBar (void)
 {
 
-}
-
-void
-MenuBar::add_ladspa_plugin(const QString& plugin_name)
-{
-    auto plugin_action = ladspa_plugins_menu->addAction(plugin_name);
-    connect(plugin_action, &QAction::triggered, this, [this, plugin_name]{ ladspa_plugin_triggered (plugin_name); });
 }
 
 void
-MenuBar::add_lv2_plugin(const QString& plugin_name)
+MenuBar::add_effect (const QString& effect_name)
 {
-    lv2_plugins_menu->addAction(plugin_name);
+  auto effect_action = effects_menu->addAction (effect_name);
+
+  auto effect_callback = [this, effect_name]{
+    on_effect_selected (effect_name);
+  };
+
+  connect (effect_action, &QAction::triggered, this, effect_callback);
 }
 
 void
-MenuBar::quit_triggered(void)
+MenuBar::on_quit_selected (void)
 {
-    emit quit_requested();
+  emit quit_selected ();
 }
 
 void
-MenuBar::ladspa_plugin_triggered(const QString& plugin_name)
+MenuBar::on_effect_selected (const QString& effect_name)
 {
-    emit ladspa_plugin_selected(plugin_name);
+  emit effect_selected (effect_name);
 }
 
 void
-MenuBar::lv2_plugin_triggered(const QString& plugin_name)
+MenuBar::on_view_documentation_selected (void)
 {
-    emit lv2_plugin_selected(plugin_name);
+  doc_browser->show ();
 }
 
 void
-MenuBar::view_documentation_triggered(void)
+MenuBar::on_report_an_issue_selected (void)
 {
-    doc_browser->show();
+  QDesktopServices::openUrl (QUrl (GNUITAR_ISSUES_LINK));
 }
 
 void
-MenuBar::report_an_issue_triggered(void)
-{
-    QDesktopServices::openUrl(QUrl(GNUITAR_ISSUES_LINK));
-}
-
-void
-MenuBar::about_triggered(void)
+MenuBar::on_about_selected (void)
 {
 
 }
--- src/gnuitar-qt/menu-bar.h
+++ src/gnuitar-qt/menu-bar.h
@@ -3,69 +3,55 @@
 
 #include <QMenuBar>
 
-#include "doc-browser.h"
+#include <gnuitar-qt/doc-browser.h>
 
-namespace Gnuitar {
+namespace Gnuitar
+{
 
-namespace Qt {
+namespace Qt
+{
 
-class MenuBar : public QMenuBar {
-
-    Q_OBJECT
-
+class MenuBar final : public QMenuBar
+{
+  Q_OBJECT
 public:
-
-    MenuBar(QWidget *parent = nullptr);
-    ~MenuBar(void);
-    void add_ladspa_plugin(const QString& plugin_name);
-    void add_lv2_plugin(const QString& plugin_name);
-
+  MenuBar (QWidget *parent = nullptr);
+  ~MenuBar (void);
+  void add_effect (const QString& plugin_name);
 private slots:
-
-    void quit_triggered(void);
-
-    void ladspa_plugin_triggered(const QString& plugin_name);
-    void lv2_plugin_triggered(const QString& plugin_name);
-
-    void view_documentation_triggered(void);
-    void report_an_issue_triggered(void);
-    void about_triggered(void);
-
+  void on_quit_selected (void);
+  void on_effect_selected (const QString& plugin_name);
+  void on_view_documentation_selected (void);
+  void on_report_an_issue_selected (void);
+  void on_about_selected (void);
 signals:
-
-    void quit_requested(void);
-    void ladspa_plugin_selected(const QString& plugin_name);
-    void lv2_plugin_selected(const QString& plugin_name);
-
+  void quit_selected (void);
+  void effect_selected (const QString& effect_name);
 private:
+  QMenu *file_menu;
+  QAction *open_preset_action;
+  QAction *save_preset_action;
+  QAction *quit_action;
 
-    QMenu *file_menu;
-    QAction *open_preset_action;
-    QAction *save_preset_action;
-    QAction *quit_action;
+  QMenu *edit_menu;
+  QAction *redo_action;
+  QAction *undo_action;
+  QAction *preferences_action;
 
-    QMenu *edit_menu;
-    QAction *redo_action;
-    QAction *undo_action;
-    QAction *preferences_action;
+  QMenu *view_menu;
+  QAction *zoom_in_action;
+  QAction *zoom_out_action;
+  QAction *normal_size_action;
+  QAction *fullscreen_action;
 
-    QMenu *view_menu;
-    QAction *zoom_in_action;
-    QAction *zoom_out_action;
-    QAction *normal_size_action;
-    QAction *fullscreen_action;
+  QMenu *effects_menu;
 
-    QMenu *plugins_menu;
-    QMenu *ladspa_plugins_menu;
-    QMenu *lv2_plugins_menu;
+  QMenu *help_menu;
+  QAction *about_action;
+  QAction *view_documentation_action;
+  QAction *report_an_issue_action;
 
-    QMenu *help_menu;
-    QAction *about_action;
-    QAction *view_documentation_action;
-    QAction *report_an_issue_action;
-
-    DocBrowser *doc_browser;
-
+  DocBrowser *doc_browser;
 }; /* class MenuBar */
 
 } /* namespace Qt */
