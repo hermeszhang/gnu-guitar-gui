6771f1cca9a20fd851caa0bdcf517f553c3d7b4d
tay10r <taylorcholberton@gmail.com>
2017-03-13 22:06:30 -0700

    Removed effect view implementation

--- src/gnuitar-qt/rack.cc
+++ src/gnuitar-qt/rack.cc
@@ -12,54 +12,6 @@ namespace Gnuitar
 namespace Qt
 {
 
-EffectView::EffectView (const Effect *effect, QWidget *parent) : QWidget(parent)
-{
-  std::string effect_name;
-  effect->get_name(effect_name);
-  effect_name = "<i><b>" + effect_name + "</i></b>";
-
-  auto effect_label = new QLabel;
-  effect_label->setTextFormat (::Qt::RichText);
-  effect_label->setText (effect_name.c_str ());
-  effect_label->setAlignment (::Qt::AlignLeft);
-
-  layout = new QHBoxLayout;
-  layout->addWidget (effect_label);
-  setLayout(layout);
-
-  show_controls (effect);
-}
-
-EffectView::EffectView (const QString& name, QWidget *parent) : QWidget(parent)
-{
-  auto effect_label = new QLabel;
-  effect_label->setTextFormat (::Qt::RichText);
-  effect_label->setText ("<i><b>" + name + "</i></b>");
-  effect_label->setAlignment (::Qt::AlignCenter);
-  layout = new QHBoxLayout;
-  layout->addWidget (effect_label);
-  setLayout (layout);
-}
-
-EffectView::~EffectView (void)
-{
-
-}
-
-void
-EffectView::show_controls (const Effect *effect)
-{
-  if (effect == nullptr)
-    throw std::invalid_argument ("effect is null");
-
-  auto control_names = effect->get_control_names ();
-  for (const auto& control_name : control_names)
-    {
-      auto control_knob = new Knob (control_name.c_str (), this);
-      layout->addWidget (control_knob);
-    }
-}
-
 Rack::Rack (QWidget *parent) : QWidget (parent)
 {
   layout = new QVBoxLayout;
--- src/gnuitar-qt/rack.h
+++ src/gnuitar-qt/rack.h
@@ -2,30 +2,15 @@
 #define GNUITAR_QT_RACK_H
 
 #include <QWidget>
-#include <QHBoxLayout>
+#include <QVBoxLayout>
 
-#include <gnuitar-qt/knob.h>
-
-#include <libgnuitar/effect.h>
+#include <gnuitar-qt/effect-view.h>
 
 namespace Gnuitar
 {
 
 namespace Qt
 {
-
-class EffectView final : public QWidget
-{
-  Q_OBJECT
-public:
-  EffectView (const Effect *effect, QWidget *parent = nullptr);
-  EffectView (const QString& name, QWidget *parent = nullptr);
-  ~EffectView (void);
-protected:
-  void show_controls (const Effect *effect);
-private:
-  QHBoxLayout *layout;
-}; /* class Effect */
 
 class Rack final : public QWidget
 {
