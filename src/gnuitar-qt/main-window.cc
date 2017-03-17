a661b69d5295206171e908708de0d20b51c962bf
tay10r <taylorcholberton@gmail.com>
2017-03-16 20:55:35 -0700

    created the effect_changed, play and stop signals ()

--- src/gnuitar-qt/main-window.cc
+++ src/gnuitar-qt/main-window.cc
@@ -23,6 +23,7 @@ MainWindow::MainWindow (QWidget *parent) : QMainWindow (parent)
   setCentralWidget (central_widget);
   setMenuBar (menu_bar);
 
+  connect (rack, &Rack::effect_changed, this, &MainWindow::on_effect_changed);
   connect (menu_bar, &MenuBar::effect_selected, this, &MainWindow::on_effect_selected);
   connect (menu_bar, &MenuBar::quit_selected, this, &MainWindow::on_quit_selected);
   connect (audio_panel, &AudioPanel::play_triggered, this, &MainWindow::on_play_selected);
@@ -35,15 +36,33 @@ MainWindow::~MainWindow (void)
 }
 
 void
+MainWindow::add_effect (EffectView *effect_view)
+{
+  rack->add_effect (effect_view);
+}
+
+void
+MainWindow::add_effect_choice (const QString& effect_name)
+{
+  menu_bar->add_effect (effect_name);
+}
+
+void
 MainWindow::on_play_selected (void)
 {
-
+  emit play_selected ();
 }
 
 void
 MainWindow::on_stop_selected (void)
 {
+  emit stop_selected ();
+}
 
+void
+MainWindow::on_effect_changed (const QString& effect_name, const QString& control_name, int control_value)
+{
+  emit effect_changed (effect_name, control_name, control_value);
 }
 
 void
--- src/gnuitar-qt/main-window.h
+++ src/gnuitar-qt/main-window.h
@@ -18,11 +18,17 @@ class MainWindow : public QMainWindow
 public:
   MainWindow (QWidget *parent = 0);
   ~MainWindow (void);
+  void add_effect (EffectView *effect_view);
+  void add_effect_choice (const QString& effect_name);
 signals:
+  void effect_changed (const QString& effect_name, const QString& control_name, int control_value);
   void effect_selected (const QString& effect_name);
+  void play_selected (void);
+  void stop_selected (void);
 private slots:
   void on_play_selected (void);
   void on_stop_selected (void);
+  void on_effect_changed (const QString& effect_name, const QString& control_name, int control_value);
   void on_effect_selected (const QString& effect_name);
   void on_quit_selected (void);
 private:
