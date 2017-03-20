14dba27293ae61812818c5637e5df1bca4902d1e
tay10r <taylorcholberton@gmail.com>
2017-03-19 23:11:06 -0700

    updated test to use a circuit checker (visitor pattern)

--- src/gnuitar-ampc/spice-parser-test.cc
+++ src/gnuitar-ampc/spice-parser-test.cc
@@ -1,38 +1,61 @@
 #include <gnuitar-ampc/spice-parser.h>
 
+#include <gnuitar-ampc/visitor.h>
+
 #include <sstream>
 
 #include <cassert>
+
+namespace
+{
+
+class CircuitChecker final : public Gnuitar::AmpC::Visitor
+{
+public:
+  ~CircuitChecker (void)
+  {
+
+  }
+  void
+  visit (const Gnuitar::AmpC::Resistor& r) noexcept
+  {
+    assert (r.get_label () == "swamp");
+    assert (r.get_input () == 1);
+    assert (r.get_output () == 3);
+  }
+  void
+  visit (const Gnuitar::AmpC::Capacitor& c) noexcept
+  {
+    assert (c.get_label () == "1");
+    assert (c.get_input () == 3);
+    assert (c.get_output () == 1);
+  }
+  void
+  visit (const Gnuitar::AmpC::Voltage&) noexcept
+  {
+
+  }
+};
+
+} /* namespace */
 
 int
 main (void)
 {
   std::stringstream source;
   source << "Rswamp 1 3 200" << std::endl;
-  source << "C1 3 4 1.00" << std::endl;
-  source << "Rload 4 2 200" << std::endl;
-  source << "Cbypass 2 1 1.00" << std::endl;
+  source << "C1 3 1 1.00" << std::endl;
 
   Gnuitar::AmpC::SpiceLexer lexer (source);
 
   Gnuitar::AmpC::SpiceParser parser (lexer);
 
-  Gnuitar::AmpC::Resistor rswamp;
-  parser.read (rswamp);
-  assert (rswamp.get_label () == "swamp");
-  assert (rswamp.get_input () == 1);
-  assert (rswamp.get_output () == 3);
-  assert (static_cast<int> (rswamp.get_resistance ()) == 200);
-
-  Gnuitar::AmpC::Capacitor c1;
-  parser.read (c1);
-  assert (c1.get_label () == "1");
-  assert (c1.get_input () == 3);
-  assert (c1.get_output () == 4);
-  assert (static_cast<int> (c1.get_capacitance ()) == 1);
-
   Gnuitar::AmpC::Circuit circuit;
   parser.read (circuit);
+
+  CircuitChecker checker;
+
+  circuit.accept (checker);
 
   return 0;
 }
