2cbe958eb9540143ece23fb2a77175a34807c5fe
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-02 12:47:40 -0400

    using a single button instead of two

--- src/audio-panel.cc
+++ src/audio-panel.cc
@@ -1,31 +1,70 @@
 #include <gnu-guitar-qt/audio-panel.h>
 
+#include <QHBoxLayout>
+#include <QPushButton>
+
+namespace {
+
+void setStopped(QPushButton *pushButton) {
+  pushButton->setText("Start");
+  pushButton->setStyleSheet("QPushButton { background: rgba(0, 255, 0, 0) }");
+}
+
+void setStarted(QPushButton *pushButton) {
+  pushButton->setText("Stop");
+  pushButton->setStyleSheet("QPushButton { background: rgba(255, 0, 0, 0) }");
+}
+
+} // namespace
+
 namespace Gnuitar {
 
 namespace Qt {
 
 AudioPanel::AudioPanel(QWidget *parent) : QWidget(parent) {
+
+  on = false;
+
+  setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
   layout = new QHBoxLayout();
+  layout->setAlignment(::Qt::AlignVCenter | ::Qt::AlignLeft);
 
-  play_button = new Button(":/icons/play.png");
-  stop_button = new Button(":/icons/stop.png");
+  powerButton = new QPushButton(this);
+  auto width = 75;
+  auto height = 50;
+  powerButton->setFixedSize(width, height);
+  setStopped(powerButton);
 
-  connect(play_button, &Button::clicked, this, &AudioPanel::on_play_triggered);
-  connect(stop_button, &Button::clicked, this, &AudioPanel::on_stop_triggered);
+  connect(powerButton, &QPushButton::clicked,
+          this, &AudioPanel::onPowerButtonClicked);
 
-  layout->addWidget(play_button);
-  layout->addWidget(stop_button);
+  layout->addWidget(powerButton);
 
   setLayout(layout);
 }
 
-AudioPanel::~AudioPanel(void) {}
+AudioPanel::~AudioPanel(void) {
+  if (powerButton != nullptr) {
+    delete powerButton;
+    powerButton = nullptr;
+  }
+  if (layout != nullptr) {
+    delete layout;
+    layout = nullptr;
+  }
+}
 
-void AudioPanel::add_control(const char *name) { (void)name; }
-
-void AudioPanel::on_play_triggered(void) { emit play_triggered(); }
-
-void AudioPanel::on_stop_triggered(void) { emit stop_triggered(); }
+void AudioPanel::onPowerButtonClicked() {
+  if (!on) {
+    setStarted(powerButton);
+    emit play_triggered();
+    on = true;
+  } else {
+    setStopped(powerButton);
+    emit stop_triggered();
+    on = false;
+  }
+}
 
 } /* namespace Qt */
 
