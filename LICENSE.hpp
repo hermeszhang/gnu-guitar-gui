9dfa45a4dc68c0b2fe199ee1cc570b9e2145b448
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-05 18:04:06 -0400

    added license header

---
+++ LICENSE.hpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2017 Taylor Holberton
+//
+//  This file is part of GNU Guitar Qt.
+//
+//  GNU Guitar Qt is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  GNU Guitar Qt is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with GNU Guitar Qt.  If not, see <http://www.gnu.org/licenses/>.
+
