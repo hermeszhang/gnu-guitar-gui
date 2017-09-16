e2bb47390eefd6b518bbe230639f92af7a374505
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-16 17:15:34 -0400

    adding binary controls too

--- src/form.cpp
+++ src/form.cpp
@@ -1,7 +1,9 @@
 #include <gnu-guitar/qt/form.hpp>
 
 #include <gnu-guitar/qt/combo-box.hpp>
+#include <gnu-guitar/qt/labelled-switch.hpp>
 
+#include <gnu-guitar/gui/binary-control.hpp>
 #include <gnu-guitar/gui/string-control.hpp>
 
 #include <QVBoxLayout>
@@ -17,6 +19,17 @@ Form::Form(QWidget *parent) : QFrame(parent) {
 
 Form::~Form() {
 
+}
+
+void Form::addControl(const Gui::BinaryControl &binaryControl) {
+
+  std::string controlName;
+  binaryControl.getName(controlName);
+
+  auto labelledSwitch = new LabelledSwitch(this);
+  labelledSwitch->setLabel(controlName.c_str());
+
+  layout->addWidget(labelledSwitch);
 }
 
 void Form::addControl(const Gui::StringControl &stringControl) {
