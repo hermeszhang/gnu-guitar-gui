66d2bb3da1b4adeb40dbcb44e8e6a5e53aae28b3
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-16 21:15:38 -0400

    made Form a base class, accepting FormVisitor

--- include/gnu-guitar/qt/form.hpp
+++ include/gnu-guitar/qt/form.hpp
@@ -16,14 +16,24 @@ class StringControl;
 
 namespace Qt {
 
-class Form final : public QFrame {
+class ComboBox;
+class FormItem;
+class FormVisitor;
+class LabelledSwitch;
+
+class Form : public QFrame {
   Q_OBJECT
 public:
   Form(QWidget *parent = nullptr);
-  ~Form();
+  virtual ~Form();
+  void accept(FormVisitor &formVisitor) const;
   void addControl(const Gui::BinaryControl &binaryControl);
   void addControl(const Gui::StringControl &stringControl);
+protected:
+  void add(ComboBox *comboBox);
+  void add(LabelledSwitch *switch_);
 private:
+  QList<FormItem*> items;
   QVBoxLayout *layout;
 };
 
