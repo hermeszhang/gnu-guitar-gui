d519b91d7ee517bc8694c753b13eff5511e9bba6
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-16 20:53:44 -0400

    accepting form visitors, emitting selected() signal

--- src/labelled-switch.cpp
+++ src/labelled-switch.cpp
@@ -1,5 +1,7 @@
 #include <gnu-guitar/qt/labelled-switch.hpp>
 
+#include <gnu-guitar/qt/form-visitor.hpp>
+
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QRadioButton>
@@ -13,6 +15,8 @@ LabelledSwitch::LabelledSwitch(QWidget *parent) : QWidget(parent) {
   label = new QLabel(this);
 
   radioButton = new QRadioButton(this);
+  connect(radioButton, &QRadioButton::pressed,
+          this, &LabelledSwitch::onSelected);
 
   layout = new QHBoxLayout(this);
   layout->addWidget(label);
@@ -23,8 +27,24 @@ LabelledSwitch::~LabelledSwitch() {
 
 }
 
+void LabelledSwitch::accept(FormVisitor &formVisitor) const {
+  formVisitor.visit(*this);
+}
+
+void LabelledSwitch::getLabel(QString &labelText) const {
+  labelText = label->text();
+}
+
 void LabelledSwitch::setLabel(const QString &labelText) {
   label->setText(labelText);
+}
+
+void LabelledSwitch::unselect() {
+  radioButton->setChecked(false);
+}
+
+void LabelledSwitch::onSelected() {
+  emit selected();
 }
 
 } // namespace Qt
