eeac81f5a0b56e90335574451f1f8ba004a3a138
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-05 13:24:32 -0400

    updated license stamp

--- LICENSE.hpp
+++ LICENSE.hpp
@@ -1,17 +1,17 @@
 // Copyright (C) 2017 Taylor Holberton
 //
-//  This file is part of GNU Guitar Qt.
+//  This file is part of GNU Guitar GUI.
 //
-//  GNU Guitar Qt is free software: you can redistribute it and/or modify
+//  GNU Guitar GUI is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
-//  GNU Guitar Qt is distributed in the hope that it will be useful,
+//  GNU Guitar GUI is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with GNU Guitar Qt.  If not, see <http://www.gnu.org/licenses/>.
+//  along with GNU Guitar GUI.  If not, see <http://www.gnu.org/licenses/>.
 
